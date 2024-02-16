401a6713e5c43ddb2307e8079d1792c7d83d0500
Tony133 <a.tripodi133@gmail.com>
2024-02-16 21:02:59 +0100

    chore: update app graphql apollo code first

--- apps/nestjs-graphql-apollo-code-first/src/graphql.options.ts
+++ apps/nestjs-graphql-apollo-code-first/src/graphql.options.ts
@@ -2,15 +2,14 @@ import { GqlOptionsFactory } from '@nestjs/graphql';
 import { Injectable } from '@nestjs/common';
 import { ApolloDriverConfig } from '@nestjs/apollo';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { dirname } from 'path';
 
 @Injectable()
 export class GraphqlOptions implements GqlOptionsFactory {
   createGqlOptions(): Promise<ApolloDriverConfig> | ApolloDriverConfig {
     return {
       autoSchemaFile: './../code-first/src/schema.gql',
-      playground: true,
-      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     };
   }
 }
